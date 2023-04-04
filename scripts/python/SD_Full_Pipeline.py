dc7ab25b6e1b3d0e0d8c3e47cab63ac14d378855
Ambrosiussen <paul@ambrosiussen.com>
2023-04-04 17:39:02 +0200

    Started refactoring pipeline to support modular framework

--- scripts/python/SD_Full_Pipeline.py
+++ scripts/python/SD_Full_Pipeline.py
@@ -1,76 +1,112 @@
-from transformers import CLIPTextModel, CLIPTokenizer
-from diffusers import AutoencoderKL, UNet2DConditionModel, PNDMScheduler
-
-# 1. Load the autoencoder model which will be used to decode the latents into image space. 
-vae = AutoencoderKL.from_pretrained("CompVis/stable-diffusion-v1-4", subfolder="vae")
-
-# 2. Load the tokenizer and text encoder to tokenize and encode the text. 
-tokenizer = CLIPTokenizer.from_pretrained("openai/clip-vit-large-patch14")
-text_encoder = CLIPTextModel.from_pretrained("openai/clip-vit-large-patch14")
-
-# 3. The UNet model for generating the latents.
-unet = UNet2DConditionModel.from_pretrained("CompVis/stable-diffusion-v1-4", subfolder="unet")
+######## TEXT PROMPT NODE ########
+ATTR_PROMPT = ["a photograph of an astronaut riding a horse"]
+######## TEXT PROMPT NODE ########
 
 
-# Creating our pre-trained scheduler
-from diffusers import LMSDiscreteScheduler
 
-scheduler = LMSDiscreteScheduler(beta_start=0.00085, beta_end=0.012, beta_schedule="scaled_linear", num_train_timesteps=1000)
 
-# Force to GPU
-torch_device = "cuda"
-vae.to(torch_device)
-text_encoder.to(torch_device)
-unet.to(torch_device)
 
+####### SYSTEM SETTINGS NODE #######
+ATTR_TORCH_DEVICE = "cuda"
+ATTR_IMAGE_HEIGHT = 512                        # default ATTR_IMAGE_HEIGHT of Stable Diffusion
+ATTR_IMAGE_WIDTH = 512                         # default ATTR_IMAGE_WIDTH of Stable Diffusion
+ATTR_NUM_INFERENCE_STEPS = 20           # Number of denoising steps
+ATTR_SEED = 44
+####### SYSTEM SETTINGS NODE #######
+
+
+
+
+
+
+
+###### TOKENIZER NODE ######
+from transformers import CLIPTokenizer
+tokenizer = CLIPTokenizer.from_pretrained("openai/clip-vit-large-patch14", local_files_only=True)
+text_input = tokenizer(ATTR_PROMPT, padding="max_length", max_length=tokenizer.model_max_length, truncation=True, return_tensors="pt")
+ATTR_INPUT_IDS = list(text_input.input_ids.cpu().numpy()[0])
+###### TOKENIZER NODE ######
+
+
+
+
+
+
+
+
+
+###### EMBEDDER NODE  ######
+from transformers import CLIPTextModel
+import torch
+import numpy
+
+__IDS = torch.from_numpy(numpy.array([ATTR_INPUT_IDS]))
+#TODO: Remove duplicate tokenizer
+batch_size = len(ATTR_PROMPT)
+text_encoder = CLIPTextModel.from_pretrained("openai/clip-vit-large-patch14", local_files_only=True)
+text_encoder.to(ATTR_TORCH_DEVICE)
+text_embeddings = text_encoder(__IDS.to(ATTR_TORCH_DEVICE))[0]
+max_length = __IDS.shape[-1]
+_tokenizer = CLIPTokenizer.from_pretrained("openai/clip-vit-large-patch14", local_files_only=True)
+uncond_input = _tokenizer([""] * batch_size, padding="max_length", max_length=max_length, return_tensors="pt")
+uncond_embeddings = text_encoder(uncond_input.input_ids.to(ATTR_TORCH_DEVICE))[0]
+text_embeddings = torch.cat([uncond_embeddings, text_embeddings])
+#print(text_embeddings.shape)
+del _tokenizer
+###### EMBEDDER NODE  ######
+
+
+
+
+
+
+
+
+
+###### LATENT NOISE NODE  ######
+# TODO: FIX THE DEPENDENCY FOR THE unet.in_channels
+from diffusers import UNet2DConditionModel
+_unet = UNet2DConditionModel.from_pretrained("CompVis/stable-diffusion-v1-4", subfolder="unet", local_files_only=True)
+_unet.to(ATTR_TORCH_DEVICE)
 
 import torch
-prompt = ["a photograph of an astronaut riding a horse"]
-
-height = 512                        # default height of Stable Diffusion
-width = 512                         # default width of Stable Diffusion
-
-num_inference_steps = 20           # Number of denoising steps
-
-guidance_scale = 7.5                # Scale for classifier-free guidance
-
-generator = torch.manual_seed(0)    # Seed generator to create the inital latent noise
-
-batch_size = len(prompt)
+batch_size = len(ATTR_PROMPT)
+generator = torch.manual_seed(ATTR_SEED)    # Seed generator to create the inital latent noise
+latents = torch.randn((batch_size, _unet.in_channels, ATTR_IMAGE_HEIGHT // 8, ATTR_IMAGE_WIDTH // 8),generator=generator,)
+del _unet
+latents = latents.to(ATTR_TORCH_DEVICE)
+###### LATENT NOISE NODE  ######
 
 
 
-text_input = tokenizer(prompt, padding="max_length", max_length=tokenizer.model_max_length, truncation=True, return_tensors="pt")
-
-text_embeddings = text_encoder(text_input.input_ids.to(torch_device))[0]
 
 
-max_length = text_input.input_ids.shape[-1]
-uncond_input = tokenizer(
-    [""] * batch_size, padding="max_length", max_length=max_length, return_tensors="pt"
-)
-uncond_embeddings = text_encoder(uncond_input.input_ids.to(torch_device))[0]
 
 
-text_embeddings = torch.cat([uncond_embeddings, text_embeddings])
-
-latents = torch.randn(
-    (batch_size, unet.in_channels, height // 8, width // 8),
-    generator=generator,
-)
-latents = latents.to(torch_device)
-
-scheduler.set_timesteps(num_inference_steps)
+###### SCHEDULER NODE #######
+# Creating our pre-trained scheduler
+from diffusers import LMSDiscreteScheduler
+scheduler = LMSDiscreteScheduler(beta_start=0.00085, beta_end=0.012, beta_schedule="scaled_linear", num_train_timesteps=1000)
+scheduler.set_timesteps(ATTR_NUM_INFERENCE_STEPS)
 
 latents = latents * scheduler.init_noise_sigma
+###### SCHEDULER NODE #######
 
 
 
+
+
+
+
+##### UNET SOLVER NODE ########
+# 3. The UNet model for generating the latents.
+from diffusers import UNet2DConditionModel
+unet = UNet2DConditionModel.from_pretrained("CompVis/stable-diffusion-v1-4", subfolder="unet", local_files_only=True)
+unet.to(ATTR_TORCH_DEVICE)
 
 from tqdm.auto import tqdm
-
-scheduler.set_timesteps(num_inference_steps)
-
+import torch
+guidance_scale = 7.5                # Scale for classifier-free guidance
 for t in tqdm(scheduler.timesteps):
     # expand the latents if we are doing classifier-free guidance to avoid doing two forward passes.
     latent_model_input = torch.cat([latents] * 2)
@@ -87,14 +123,32 @@ for t in tqdm(scheduler.timesteps):
 
     # compute the previous noisy sample x_t -> x_t-1
     latents = scheduler.step(noise_pred, t, latents).prev_sample
+##### UNET SOLVER NODE ########
 
 
 
+
+
+
+######## IMAGE DECODER NODE ########
 # scale and decode the image latents with vae
+# 1. Load the autoencoder model which will be used to decode the latents into image space. 
+from diffusers import AutoencoderKL
+import torch
+vae = AutoencoderKL.from_pretrained("CompVis/stable-diffusion-v1-4", subfolder="vae", local_files_only=True)
+vae.to(ATTR_TORCH_DEVICE)
+
 latents = 1 / 0.18215 * latents
 with torch.no_grad():
     image = vae.decode(latents).sample
+######## IMAGE DECODER NODE ########
 
+
+
+
+
+
+##### UTILITY TO WRITE IMAGE TO DISK #######
 from PIL import Image
 image = (image / 2 + 0.5).clamp(0, 1)
 image = image.detach().cpu().permute(0, 2, 3, 1).numpy()
@@ -102,4 +156,4 @@ images = (image * 255).round().astype("uint8")
 pil_images = [Image.fromarray(image) for image in images]
 pil_images[0].show()
 pil_images[0].save(r"C:\Users\Paul\OneDrive - Bismuth Consultancy B.V\BISMUTH_CONSULTANCY\Work\Projects\Monaco\Toolset\MLOPs\scripts\pythongeeks.jpg")
-
+##### UTILITY TO WRITE IMAGE TO DISK #######
