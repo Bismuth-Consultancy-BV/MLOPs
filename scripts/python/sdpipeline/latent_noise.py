dccef5c5a35c49f3da097f07a162e12b9c87e627
Ambrosiussen <paul@ambrosiussen.com>
2023-04-05 15:22:07 +0200

    removedUNET dependency in latent noise generate

--- scripts/python/sdpipeline/latent_noise.py
+++ scripts/python/sdpipeline/latent_noise.py
@@ -1,12 +1,9 @@
 from diffusers import UNet2DConditionModel
 import torch
 
-# TODO: FIX THE DEPENDENCY FOR THE unet.in_channels
 def run(noise_seed, width, height, torch_device):
-	_unet = UNet2DConditionModel.from_pretrained("CompVis/stable-diffusion-v1-4", subfolder="unet", local_files_only=True)
-	_unet.to(torch_device)
 	generator = torch.manual_seed(noise_seed)    # Seed generator to create the inital latent noise
-	latents = torch.randn((1, _unet.in_channels, height // 8, width // 8),generator=generator,)
+	latents = torch.randn((1, 4, height // 8, width // 8),generator=generator,)
 	latents = latents.to(torch_device)
 	ATTR_LATENTS = latents.cpu().numpy()[0]
 	return ATTR_LATENTS
