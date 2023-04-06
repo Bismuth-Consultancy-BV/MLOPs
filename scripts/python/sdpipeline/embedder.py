cfed380394d12ebce4d9c014be444ff185a1ce64
Ambrosiussen <paul@ambrosiussen.com>
2023-04-06 23:15:30 +0200

    text encoder now supports negative subtractive prompts. Image decoder width and height parm back again

--- scripts/python/sdpipeline/embedder.py
+++ scripts/python/sdpipeline/embedder.py
@@ -1,34 +1,25 @@
 ###### EMBEDDER NODE  ######
 from transformers import CLIPTextModel
-from transformers import CLIPTokenizer
 import torch
 import numpy
-
 import logging
 logging.disable(logging.WARNING)  
 
-def run(input_ids, torch_device, model="openai/clip-vit-large-patch14", local_cache_only=True):
-    __IDS = torch.from_numpy(numpy.array([input_ids], dtype=numpy.int64))
-    #TODO: Remove duplicate tokenizer
+def run(input_ids_positive, input_ids_negative, torch_device, model="openai/clip-vit-large-patch14", local_cache_only=True):
     text_encoder = CLIPTextModel.from_pretrained(model, subfolder="text_encoder", local_files_only=local_cache_only)
     text_encoder.to(torch_device)
     
     with torch.no_grad():
-        text_embeddings = text_encoder(__IDS.to(torch_device))[0]
+        __IDS = torch.from_numpy(numpy.array([input_ids_positive], dtype=numpy.int64))
+        positive_text_embeddings = text_encoder(__IDS.to(torch_device))[0]
 
-    max_length = __IDS.shape[-1]
-    _tokenizer = CLIPTokenizer.from_pretrained(model, subfolder="tokenizer", local_files_only=local_cache_only)
-    uncond_input = _tokenizer([""] * 1, padding="max_length", max_length=max_length, return_tensors="pt")
-    
-    with torch.no_grad():
-        uncond_embeddings = text_encoder(uncond_input.input_ids.to(torch_device))[0]
+        __IDS2 = torch.from_numpy(numpy.array([input_ids_negative], dtype=numpy.int64))
+        negative_text_embeddings = text_encoder(__IDS2.to(torch_device))[0]
 
-    text_embeddings = torch.cat([uncond_embeddings, text_embeddings])
+    all_text_embeddings = torch.cat([negative_text_embeddings, positive_text_embeddings]).detach().cpu().numpy()
 
-    _embeddings = text_embeddings.detach().cpu().numpy()
-
-    ATTR_UNCONDITIONAL_EMBEDDING = _embeddings[0]
-    ATTR_CONDITIONAL_EMBEDDING = _embeddings[1]
+    ATTR_UNCONDITIONAL_EMBEDDING = all_text_embeddings[0]
+    ATTR_CONDITIONAL_EMBEDDING = all_text_embeddings[1]
     ATTR_EMBEDDING_SHAPE = list(ATTR_UNCONDITIONAL_EMBEDDING.shape)
     ATTR_UNCONDITIONAL_EMBEDDING = ATTR_UNCONDITIONAL_EMBEDDING.flatten()
     ATTR_CONDITIONAL_EMBEDDING = ATTR_CONDITIONAL_EMBEDDING.flatten()
@@ -37,5 +28,4 @@ def run(input_ids, torch_device, model="openai/clip-vit-large-patch14", local_ca
     embedding["unconditional_embedding"] = ATTR_UNCONDITIONAL_EMBEDDING
     embedding["conditional_embedding"] = ATTR_CONDITIONAL_EMBEDDING
     embedding["tensor_shape"] = ATTR_EMBEDDING_SHAPE
-    return embedding 
-###### EMBEDDER NODE  ######
+    return embedding
