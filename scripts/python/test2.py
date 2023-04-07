5265b4aa8ef43c1db12c3d9901097cf9b1525d18
Ambrosiussen <paul@ambrosiussen.com>
2023-04-07 13:32:05 +0200

    Create test2.py

---
+++ scripts/python/test2.py
@@ -0,0 +1,163 @@
+import os
+os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "max_split_size_mb:400"
+
+import torch, logging
+
+## disable warnings
+logging.disable(logging.WARNING)  
+
+## Imaging  library
+from PIL import Image
+from torchvision import transforms as tfms
+
+
+## Basic libraries
+from fastdownload import FastDownload
+import numpy as np
+from tqdm.auto import tqdm
+import matplotlib.pyplot as plt
+# %matplotlib inline
+from IPython.display import display
+import shutil
+import os
+
+## For video display
+from IPython.display import HTML
+from base64 import b64encode
+
+
+
+
+
+## Import the CLIP artifacts 
+from transformers import CLIPTextModel, CLIPTokenizer
+from diffusers import AutoencoderKL, UNet2DConditionModel, LMSDiscreteScheduler
+
+model = r"C:\BISMUTH\Work\Projects\Monaco\checkpoints\SD_2_1\stable-diffusion-2-1"
+r"C:\Users\Paul\OneDrive - Bismuth Consultancy B.V\Desktop\EPC_Profile.jpg")
+
+
+## Initiating tokenizer and encoder.
+tokenizer = CLIPTokenizer.from_pretrained(model, local_files_only=True, subfolder="tokenizer", torch_dtype=torch.float16)
+text_encoder = CLIPTextModel.from_pretrained(model, local_files_only=True, subfolder="text_encoder", torch_dtype=torch.float16).to("cuda")
+
+## Initiating the VAE
+vae = AutoencoderKL.from_pretrained(model, local_files_only=True, subfolder="vae", torch_dtype=torch.float16).to("cuda")
+
+## Initializing a scheduler and Setting number of sampling steps
+scheduler = LMSDiscreteScheduler(beta_start=0.00085, beta_end=0.012, beta_schedule="scaled_linear", num_train_timesteps=1000)
+scheduler.set_timesteps(50)
+
+## Initializing the U-Net model
+unet = UNet2DConditionModel.from_pretrained(model, local_files_only=True, subfolder="unet", torch_dtype=torch.float16).to("cuda")
+
+## Helper functions
+def load_image(p):
+    '''
+    Function to load images from a defined path
+    '''
+    return Image.open(p).convert('RGB').resize((768,768))
+
+def pil_to_latents(image):
+    '''
+    Function to convert image to latents
+    '''
+    init_image = tfms.ToTensor()(image).unsqueeze(0) * 2.0 - 1.0
+    init_image = init_image.to(device="cuda", dtype=torch.float16) 
+    init_latent_dist = vae.encode(init_image).latent_dist.sample() * 0.18215
+    return init_latent_dist
+
+def latents_to_pil(latents):
+    '''
+    Function to convert latents to images
+    '''
+    latents = (1 / 0.18215) * latents
+    with torch.no_grad():
+        image = vae.decode(latents).sample
+
+
+    image = (image / 2 + 0.5).clamp(0, 1)
+    image = image.detach().cpu().permute(0, 2, 3, 1).numpy()
+    images = (image * 255).round().astype("uint8")
+    pil_images = [Image.fromarray(image) for image in images]
+    return pil_images
+
+def text_enc(prompts, maxlen=None):
+    '''
+    A function to take a texual promt and convert it into embeddings
+    '''
+    if maxlen is None: maxlen = tokenizer.model_max_length
+    inp = tokenizer(prompts, padding="max_length", max_length=maxlen, truncation=True, return_tensors="pt") 
+    return text_encoder(inp.input_ids.to("cuda"))[0].half()
+
+
+def prompt_2_img_i2i(prompt, init_img, neg_prompts=None, g=7.5, seed=100, strength =0.8, steps=10, dim=768, save_int=False):
+    """
+    Diffusion process to convert prompt to image
+    """
+    # Converting textual prompts to embedding
+    text = text_enc(prompt) 
+
+    # Adding negative prompt condition
+    if not neg_prompts: uncond =  text_enc([""] * 1, text.shape[1])
+    # Adding an unconditional prompt , helps in the generation process
+    else: uncond =  text_enc(neg_prompts, text.shape[1])
+    emb = torch.cat([uncond, text])
+
+
+    # Setting the seed
+    if seed: torch.manual_seed(seed)
+    
+    # Setting number of steps in scheduler
+    scheduler.set_timesteps(steps)
+    
+    # Convert the seed image to latent
+    init_latents = pil_to_latents(init_img)
+    
+    # Figuring initial time step based on strength
+    init_timestep = int(steps * strength) 
+    timesteps = scheduler.timesteps[-init_timestep]
+    print(timesteps)
+    timesteps = torch.tensor([timesteps], device="cuda")
+    
+    # Adding noise to the latents 
+    print(init_latents.shape)
+    noise = torch.randn(init_latents.shape, generator=None, device="cuda", dtype=init_latents.dtype)
+    init_latents = scheduler.add_noise(init_latents, noise, timesteps)
+    latents = init_latents
+    
+    # Computing the timestep to start the diffusion loop
+    t_start = max(steps - init_timestep, 0)
+    timesteps = scheduler.timesteps[t_start:].to("cuda")
+    
+    # Iterating through defined steps
+    for i,ts in enumerate(tqdm(timesteps)):
+        # We need to scale the i/p latents to match the variance
+        inp = scheduler.scale_model_input(torch.cat([latents] * 2), ts)
+        
+        # Predicting noise residual using U-Net
+        with torch.no_grad(): u,t = unet(inp, ts, encoder_hidden_states=emb).sample.chunk(2)
+            
+        # Performing Guidance
+        pred = u + g*(t-u)
+        
+        # Conditioning  the latents
+        latents = scheduler.step(pred, ts, latents).prev_sample
+        
+        # Saving intermediate images
+        if save_int: 
+            if not os.path.exists(f'./steps'):
+                os.mkdir(f'./steps')
+            latents_to_pil(latents)[0].save(f'steps/{i:04}.jpeg')
+
+    # del scheduler
+            
+    # Returning the latent representation to output an image of 3x512x512
+    del text
+    del timesteps
+    
+    return latents_to_pil(latents)
+
+
+image = prompt_2_img_i2i(["man with beard"], load_image(IMAGE)
+image[0].show()
