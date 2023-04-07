f5583a2068a5ee182b4f75ed45aa2a8ceac79a25
Ambrosiussen <paul@ambrosiussen.com>
2023-04-07 15:42:59 +0200

    scheduler now also backwards compatible

--- scripts/python/sdpipeline/scheduler.py
+++ scripts/python/sdpipeline/scheduler.py
@@ -3,10 +3,16 @@ import json
 import torch
 import numpy
 
-def run(input_latents, guided_latents, guiding_strength, guiding_seed, inference_steps, torch_device, model="CompVis/stable-diffusion-v1-4", local_cache_only=True):
-    scheduler_object = LMSDiscreteScheduler.from_pretrained(model, subfolder="scheduler", local_files_only=local_cache_only)
+def run(input_latents, latent_dimension, guided_latents, guiding_strength, guiding_seed, inference_steps, torch_device, model="CompVis/stable-diffusion-v1-4", local_cache_only=True):
+    try:
+        scheduler_object = LMSDiscreteScheduler.from_pretrained(model, subfolder="scheduler", local_files_only=local_cache_only)
+    except OSError:
+        scheduler_object = LMSDiscreteScheduler.from_pretrained(model, local_files_only=local_cache_only)
+    except Exception as err:
+        print(f"Unexpected {err=}, {type(err)=}")
+
     scheduler_object.set_timesteps(inference_steps)
-    __LATENTS = torch.from_numpy(numpy.array([input_latents.reshape(4, 96, 96)])).to(torch_device)
+    __LATENTS = torch.from_numpy(numpy.array([input_latents.reshape(4, latent_dimension[0], latent_dimension[1])])).to(torch_device)
     
     scheduler = {}
     t_start = -1
@@ -15,7 +21,7 @@ def run(input_latents, guided_latents, guiding_strength, guiding_seed, inference
          #, dtype=numpy.float64
         ATTR_SCHEDULER_LATENTS = __LATENTS * scheduler_object.init_noise_sigma
     else:
-        __GUIDEDLATENTS = torch.from_numpy(numpy.array([guided_latents.reshape(4, 96, 96)])).to(torch_device)
+        __GUIDEDLATENTS = torch.from_numpy(numpy.array([guided_latents.reshape(4, latent_dimension[0], latent_dimension[1])])).to(torch_device)
         # Figuring initial time step based on strength
         init_timestep = int(inference_steps * guiding_strength) 
 
