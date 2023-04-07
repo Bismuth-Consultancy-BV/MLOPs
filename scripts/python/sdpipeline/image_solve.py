38bd9c6ccec2f893793eb0e3f2141b3d6737e867
Ambrosiussen <paul@ambrosiussen.com>
2023-04-07 21:17:13 +0200

    solver now has progress bar and is interruptable

--- scripts/python/sdpipeline/image_solve.py
+++ scripts/python/sdpipeline/image_solve.py
@@ -4,6 +4,7 @@ from . import schedulers_lookup
 import torch
 import json
 import numpy
+import hou
 
 def run(iference_steps, latent_dimension, input_embeddings, cfg_scale, input_scheduler, torch_device, model="CompVis/stable-diffusion-v1-4", local_cache_only=True):
 
@@ -30,24 +31,25 @@ def run(iference_steps, latent_dimension, input_embeddings, cfg_scale, input_sch
     if init_timesteps >= 0:
         timesteps = __scheduler.timesteps[init_timesteps:].to(torch_device)
 
-    for t in tqdm(timesteps):
-        # expand the latents if we are doing classifier-free guidance to avoid doing two forward passes.
-        latent_model_input = torch.cat([ATTR_UNET_LATENTS] * 2)
-        latent_model_input = __scheduler.scale_model_input(latent_model_input, timestep=t)
+    with hou.InterruptableOperation("Solving Stable Diffusion", open_interrupt_dialog=True) as operation:
+        for i, t in enumerate(tqdm(timesteps, disable=True)):
+            # expand the latents if we are doing classifier-free guidance to avoid doing two forward passes.
+            latent_model_input = torch.cat([ATTR_UNET_LATENTS] * 2)
+            latent_model_input = __scheduler.scale_model_input(latent_model_input, timestep=t)
 
-        # predict the noise residual
-        with torch.no_grad():
-            noise_pred = unet(latent_model_input, t, encoder_hidden_states=_text_embedding).sample
+            # predict the noise residual
+            with torch.no_grad():
+                noise_pred = unet(latent_model_input, t, encoder_hidden_states=_text_embedding).sample
 
-        # perform guidance
-        noise_pred_uncond, noise_pred_text = noise_pred.chunk(2)
-        noise_pred = noise_pred_uncond + guidance_scale * (noise_pred_text - noise_pred_uncond)
+            # perform guidance
+            noise_pred_uncond, noise_pred_text = noise_pred.chunk(2)
+            noise_pred = noise_pred_uncond + guidance_scale * (noise_pred_text - noise_pred_uncond)
 
-        # compute the previous noisy sample x_t -> x_t-1
-        ATTR_UNET_LATENTS = __scheduler.step(noise_pred, t, ATTR_UNET_LATENTS).prev_sample
+            # compute the previous noisy sample x_t -> x_t-1
+            ATTR_UNET_LATENTS = __scheduler.step(noise_pred, t, ATTR_UNET_LATENTS).prev_sample
+            operation.updateProgress(i/len(timesteps))
     
 
     ATTR_UNET_OUT_LATENTS = ATTR_UNET_LATENTS.cpu().numpy()[0]
     return ATTR_UNET_OUT_LATENTS
     ##### UNET SOLVER NODE ########
-
