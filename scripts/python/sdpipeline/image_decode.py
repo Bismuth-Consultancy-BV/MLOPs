321268bbead70d05a6204ad5da6e6483025bee58
Ambrosiussen <paul@ambrosiussen.com>
2023-04-07 15:44:36 +0200

    image decoder now also backwards compatible

--- scripts/python/sdpipeline/image_decode.py
+++ scripts/python/sdpipeline/image_decode.py
@@ -2,14 +2,14 @@ from diffusers import AutoencoderKL
 import torch
 import numpy
 
-def run(input_latents, torch_device, model="CompVis/stable-diffusion-v1-4", local_cache_only=True):
+def run(input_latents, latent_dimension, torch_device, model="CompVis/stable-diffusion-v1-4", local_cache_only=True):
     # scale and decode the image latents with vae
     # 1. Load the autoencoder model which will be used to decode the latents into image space. 
 
     vae = AutoencoderKL.from_pretrained(model, subfolder="vae", local_files_only=local_cache_only)
     vae.to(torch_device)
 
-    __LATENTS = torch.from_numpy(numpy.array([input_latents.reshape(4, 96, 96)])).to(torch_device)
+    __LATENTS = torch.from_numpy(numpy.array([input_latents.reshape(4, latent_dimension[0], latent_dimension[1])])).to(torch_device)
     __LATENTS = (1.0 / 0.18215) * __LATENTS
     with torch.no_grad():
         image = vae.decode(__LATENTS).sample
