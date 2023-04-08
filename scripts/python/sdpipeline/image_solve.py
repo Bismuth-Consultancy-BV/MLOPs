a1de09357003374622946b2a1f5da2a4de51423f
Ambrosiussen <paul@ambrosiussen.com>
2023-04-08 19:39:57 +0200

    Update image_solve.py

--- scripts/python/sdpipeline/image_solve.py
+++ scripts/python/sdpipeline/image_solve.py
@@ -6,21 +6,19 @@ import json
 import numpy
 import hou
 
-def run(iference_steps, latent_dimension, input_embeddings, attention_slicing, cfg_scale, input_scheduler, torch_device, model="CompVis/stable-diffusion-v1-4", local_cache_only=True):
+def run(inference_steps, latent_dimension, input_embeddings, mask_latents, attention_slicing, guidance_scale, input_scheduler, torch_device, model="CompVis/stable-diffusion-v1-4", local_cache_only=True):
 
     unet = UNet2DConditionModel.from_pretrained(model, subfolder="unet", local_files_only=local_cache_only)
-    
+    unet.to(torch_device)
+
     if attention_slicing:
         unet.set_attention_slice("auto")
 
-
-    unet.to(torch_device)
-
-    guidance_scale = cfg_scale                # Scale for classifier-free guidance
     ATTR_UNET_LATENTS = torch.from_numpy(numpy.array([input_scheduler["latents"].reshape(4, latent_dimension[0], latent_dimension[1])])).to(torch_device) 
-    _unconditional_embedding = numpy.array(input_embeddings["unconditional_embedding"]).reshape(input_embeddings["tensor_shape"])
-    _conditional_embedding = numpy.array(input_embeddings["conditional_embedding"]).reshape(input_embeddings["tensor_shape"])
-    _text_embedding = torch.from_numpy(numpy.array([_unconditional_embedding, _conditional_embedding])).to(torch_device)
+    text_embeddings = numpy.array(input_embeddings["conditional_embedding"]).reshape(input_embeddings["tensor_shape"])
+    uncond_embeddings = numpy.array(input_embeddings["unconditional_embedding"]).reshape(input_embeddings["tensor_shape"])
+    
+    text_embeddings = torch.from_numpy(numpy.array([uncond_embeddings, text_embeddings])).to(torch_device)
 
     scheduler_config = input_scheduler["config"]
     init_timesteps = scheduler_config["init_timesteps"]
@@ -28,33 +26,34 @@ def run(iference_steps, latent_dimension, input_embeddings, attention_slicing, c
     del scheduler_config["init_timesteps"]
     del scheduler_config["type"]
 
-    __scheduler = schedulers_lookup.schedulers[scheduler_type].from_config(scheduler_config)
-    __scheduler.set_timesteps(iference_steps)
+    scheduler = schedulers_lookup.schedulers[scheduler_type].from_config(scheduler_config)
+    scheduler.set_timesteps(inference_steps)
 
     
-    timesteps = __scheduler.timesteps
+    timesteps = scheduler.timesteps
     if init_timesteps >= 0:
-        timesteps = __scheduler.timesteps[init_timesteps:].to(torch_device)
+        timesteps = scheduler.timesteps[init_timesteps:].to(torch_device)
 
+    latents = ATTR_UNET_LATENTS
     with hou.InterruptableOperation("Solving Stable Diffusion", open_interrupt_dialog=True) as operation:
         for i, t in enumerate(tqdm(timesteps, disable=True)):
             # expand the latents if we are doing classifier-free guidance to avoid doing two forward passes.
-            latent_model_input = torch.cat([ATTR_UNET_LATENTS] * 2)
-            latent_model_input = __scheduler.scale_model_input(latent_model_input, timestep=t)
+            latent_model_input = torch.cat([latents] * 2)
+            latent_model_input = scheduler.scale_model_input(latent_model_input, timestep=t)
 
             # predict the noise residual
             with torch.no_grad():
-                noise_pred = unet(latent_model_input, t, encoder_hidden_states=_text_embedding).sample
+                noise_pred = unet(latent_model_input, t, encoder_hidden_states=text_embeddings).sample
 
             # perform guidance
             noise_pred_uncond, noise_pred_text = noise_pred.chunk(2)
             noise_pred = noise_pred_uncond + guidance_scale * (noise_pred_text - noise_pred_uncond)
 
             # compute the previous noisy sample x_t -> x_t-1
-            ATTR_UNET_LATENTS = __scheduler.step(noise_pred, t, ATTR_UNET_LATENTS).prev_sample
+            latents = scheduler.step(noise_pred, t, latents).prev_sample
             operation.updateProgress(i/len(timesteps))
     
 
-    ATTR_UNET_OUT_LATENTS = ATTR_UNET_LATENTS.cpu().numpy()[0]
+    ATTR_UNET_OUT_LATENTS = latents.cpu().numpy()[0]
     return ATTR_UNET_OUT_LATENTS
     ##### UNET SOLVER NODE ########
