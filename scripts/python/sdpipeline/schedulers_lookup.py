c129ea99b32a1ef385dd2359e4b0f0e67589cdc2
moedeldiho <moritz.schwind@googlemail.com>
2023-04-08 11:06:05 +0200

    added additional schedulers which work out of box

--- scripts/python/sdpipeline/schedulers_lookup.py
+++ scripts/python/sdpipeline/schedulers_lookup.py
@@ -1,9 +1,23 @@
-from diffusers import EulerDiscreteScheduler, EulerAncestralDiscreteScheduler, LMSDiscreteScheduler, DDIMScheduler, UniPCMultistepScheduler
+from diffusers import DDIMScheduler,DDPMScheduler,DEISMultistepScheduler,DPMSolverMultistepScheduler,DPMSolverSinglestepScheduler,EulerAncestralDiscreteScheduler,EulerDiscreteScheduler,PNDMScheduler,UniPCMultistepScheduler,LMSDiscreteScheduler
 
 schedulers = {
 	"unipc" : UniPCMultistepScheduler,
 	"euler_discrete" : EulerDiscreteScheduler,
 	"euler_ancestral" : EulerAncestralDiscreteScheduler,
 	"lms_discrete" : LMSDiscreteScheduler,
-	"ddim" : DDIMScheduler
+	"ddim" : DDIMScheduler,
+	#"DDIMInverse" : DDIMInverseScheduler,
+	"DDPM" : DDPMScheduler,
+	"DEIS_Multistep" : DEISMultistepScheduler,
+	"DPM_Multistep" : DPMSolverMultistepScheduler,
+	"DPM_Singlestep" : DPMSolverSinglestepScheduler,
+	#"HeunDiscrete" : HeunDiscreteScheduler,
+	#"IPNDM" : IPNDMScheduler,
+	#"KDPM2Ancestral" : KDPM2AncestralDiscreteScheduler,
+	#"KDPM2" : KDPM2DiscreteScheduler,
+	#"KarrasVe" : KarrasVeScheduler,
+	"PNDM" : PNDMScheduler,
+	#"RePaint" : RePaintScheduler,
+	#"UnCLIP" : UnCLIPScheduler,
+	#"VQDiffusion" : VQDiffusionScheduler
 }
