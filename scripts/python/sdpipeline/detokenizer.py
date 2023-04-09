0a40a6b50cfb45927b671a6aac36fede2d9d7a67
Ambrosiussen <paul@ambrosiussen.com>
2023-04-09 21:01:55 +0200

    refactor detokenizer

--- scripts/python/sdpipeline/detokenizer.py
+++ scripts/python/sdpipeline/detokenizer.py
@@ -1,6 +1,6 @@
 from transformers import CLIPTokenizer
 
-def run(input_ids, skip_special_tokens=False, model="openai/clip-vit-large-patch14", local_cache_only=True):
+def run(input_ids, skip_special_tokens, model, local_cache_only=True):
     tokenizer = CLIPTokenizer.from_pretrained(model, subfolder="tokenizer", local_files_only=local_cache_only)
     string = tokenizer.decode(input_ids, skip_special_tokens)
     return string
