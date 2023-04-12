c4c1833888567d7efdb0e02de657fffd8d6a9f93
Paul Ambrosiussen <paul@ambrosiussen.com>
2023-04-12 13:50:50 +0200

    Update tokenizer.py

--- scripts/python/sdpipeline/tokenizer.py
+++ scripts/python/sdpipeline/tokenizer.py
@@ -6,7 +6,7 @@ def run(text_prompt, model, local_cache_only):
     except OSError as error:
         tokenizer = CLIPTokenizer.from_pretrained(model, local_files_only=local_cache_only)
     except Exception as err:
-        print(f"Unexpected {err=}, {type(err)=}")
+        print(f"Unexpected {err}, {type(err)}")
 
     text_input = tokenizer(text_prompt, padding="max_length", max_length=tokenizer.model_max_length, truncation=True, return_tensors="pt")
     input_ids = list(text_input.input_ids.cpu().numpy()[0])
