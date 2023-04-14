d37c0c7177fd315bf6400b80fa887aa31d4fd6a1
Paul Ambrosiussen <paul@ambrosiussen.com>
2023-04-14 14:17:08 +0200

    removed 2nd input for image encoder since we now do masking in a better way

--- scripts/python/sdpipeline/image_encode.py
+++ scripts/python/sdpipeline/image_encode.py
@@ -2,11 +2,11 @@ from diffusers import AutoencoderKL
 import torch
 
 
-def run(input_vectors, mask, torch_device, model, local_cache_only=True):
+def run(input_vectors, torch_device, model, local_cache_only=True):
     vae = AutoencoderKL.from_pretrained(model, subfolder="vae", local_files_only=local_cache_only)
     vae.to(torch_device)
     input_vectors =  (torch.from_numpy(input_vectors) * 2.0) - 1.0
-    input_vectors = input_vectors * (1.0-mask)
+    #input_vectors = input_vectors * (1.0-mask)
 
     input_vectors = input_vectors.to(torch_device)
 
