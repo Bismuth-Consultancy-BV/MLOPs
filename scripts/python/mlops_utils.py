c69105a16b707049c75557c0df00b50ff1077fad
Paul Ambrosiussen <paul@ambrosiussen.com>
2023-04-18 21:33:30 +0200

    fixed image_dimension attribute sometimes being float other times int

--- scripts/python/mlops_utils.py
+++ scripts/python/mlops_utils.py
@@ -3,16 +3,16 @@ import openai
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 def generate_gpt_code_from_prompt(prompt, wrapper, model="gpt-3.5-turbo"):
-  completion = openai.ChatCompletion.create(
-    model=model,
-    messages=[
-      {"role": "user", "content": f"{wrapper} {prompt}"}
-    ]
-  )
-  return completion.choices[0].message.content
+    completion = openai.ChatCompletion.create(
+        model=model,
+        messages=[
+            {"role": "user", "content": f"{wrapper} {prompt}"}
+        ]
+    )
+    return completion.choices[0].message.content
 
 def is_relevant_parm(kwargs, parmtype):
-  if parmtype == "wrangle":
-    if len(kwargs["parms"]) == 0:
-      return False
-  return True
+    if parmtype == "wrangle":
+        if len(kwargs["parms"]) == 0:
+            return False
+    return True
