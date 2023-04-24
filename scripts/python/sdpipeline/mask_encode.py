cdbb2f9aa194a3051ce8a13e426c6efe96d8cfea
Paul Ambrosiussen <paul@ambrosiussen.com>
2023-04-24 14:48:03 +0200

    Update mask_encode.py

--- scripts/python/sdpipeline/mask_encode.py
+++ scripts/python/sdpipeline/mask_encode.py
@@ -1,5 +1,6 @@
 import numpy
-from scipy import ndimage
+from skimage.transform import resize
+
 
 def run(input_mask, latent_width, latent_height, invert):
 
@@ -7,8 +8,7 @@ def run(input_mask, latent_width, latent_height, invert):
     mask[mask < 0.5] = 0.0
     mask[mask >= 0.5] = 1.0
 
-    zoom_factors = [new_dim / old_dim for new_dim, old_dim in zip((latent_width, latent_height), mask.shape)]
-    resampled_data = ndimage.zoom(mask, zoom_factors, order=3)  # order=1 for linear interpolation
+    resampled_data = resize(mask, (latent_width, latent_height))
     mask = numpy.tile(resampled_data,(4,1,1))
 
     if invert:
