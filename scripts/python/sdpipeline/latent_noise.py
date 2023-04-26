ff5c4e63e12cf1f76bd726cd9302327018077449
Paul Ambrosiussen <paul@ambrosiussen.com>
2023-04-26 20:10:05 +0200

    refactor on latent noise generate

--- scripts/python/sdpipeline/latent_noise.py
+++ scripts/python/sdpipeline/latent_noise.py
@@ -1,9 +1,8 @@
-from diffusers import UNet2DConditionModel
 import torch
 
 def run(noise_seed, width, height, torch_device):
-	generator = torch.manual_seed(noise_seed)    # Seed generator to create the inital latent noise
-	latents = torch.randn((1, 4, height // 8, width // 8),generator=generator,)
-	latents = latents.to(torch_device)
-	ATTR_LATENTS = latents.cpu().numpy()[0]
-	return ATTR_LATENTS
+    generator = torch.manual_seed(noise_seed)    # Seed generator to create the inital latent noise
+    latents = torch.randn((4, height // 8, width // 8),generator=generator,)
+    latents = latents.to(torch_device)
+    latents = latents.cpu().numpy()
+    return latents
