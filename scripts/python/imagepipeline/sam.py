bf74ccd49888271cfdcaee129961f8e8c1dd1463
Paul Ambrosiussen <paul@ambrosiussen.com>
2023-05-08 17:14:26 +0200

    Sam input points support labels

--- scripts/python/imagepipeline/sam.py
+++ scripts/python/imagepipeline/sam.py
@@ -3,7 +3,7 @@ import torch
 import numpy
 from transformers import pipeline, SamModel, SamProcessor
 
-def run(model, input_colors, points, torch_device, local_cache_only):
+def run(model, input_colors, points, labels, torch_device, local_cache_only):
 
     image = mlops_image_utils.colors_numpy_array_to_pil(input_colors).convert("RGB")
     if torch_device == "cuda":
@@ -14,7 +14,7 @@ def run(model, input_colors, points, torch_device, local_cache_only):
             torch_device += ":0"
 
         generator = pipeline("mask-generation", model=model, device=torch_device)
-        
+
         with torch.no_grad():
             outputs = generator(image, points_per_batch=64)
 
@@ -28,16 +28,13 @@ def run(model, input_colors, points, torch_device, local_cache_only):
             out += value * mask
 
     else:
-        sam_model = SamModel.from_pretrained(model).to(torch_device)
-        processor = SamProcessor.from_pretrained(model)
-
-        inputs = processor(image, input_points=[points], return_tensors="pt").to(torch_device)
+        sam_model = SamModel.from_pretrained(model, local_files_only=local_cache_only).to(torch_device)
+        processor = SamProcessor.from_pretrained(model, local_files_only=local_cache_only)
+        inputs = processor(image, input_points=[points], input_labels=[labels], return_tensors="pt").to(torch_device)
         with torch.no_grad():
             outputs = sam_model(**inputs)
 
-        masks = processor.image_processor.post_process_masks(
-            outputs.pred_masks.cpu(), inputs["original_sizes"].cpu(), inputs["reshaped_input_sizes"].cpu()
-        )
+        masks = processor.image_processor.post_process_masks(outputs.pred_masks.cpu(), inputs["original_sizes"].cpu(), inputs["reshaped_input_sizes"].cpu())
         mask = masks[0][0].numpy()
         mask = mask[0, :, :]
         dimension = mask.shape
