ac73d38f9a6dd0b6c97d896e7152b169eed63fb6
moedeldiho <moritz.schwind@googlemail.com>
2023-05-17 14:30:35 +0200

    clamping cosine similarity to 0. - 1. instead of remapping from -1/1 to 0/1
    paul hated it

--- scripts/python/imagepipeline/similarity.py
+++ scripts/python/imagepipeline/similarity.py
@@ -30,7 +30,8 @@ def clip_embedding_similarity(image1, image2, model_path):
 	# Calculate the cosine similarity between the normalized tensors
 	similarity = torch.matmul(normalized_tensor1, normalized_tensor2.transpose(0, 1))
 	sim = similarity.item()
-	sim = (sim +  1.0) * .5
+	#sim = (sim +  1.0) * .5
+	sim = max(sim, 0.0)
 
 	return sim
 
@@ -46,7 +47,8 @@ def ssim(pil_image1, pil_image2, data_range=1.0, kernel_size=11, sigma=1.5, k1=0
     ssim_value = ssim_value_tensor.item()
 
     # remap cosine similarity (-1. to +1.) into 0 to 1 range
-    ssim_value = (ssim_value + 1.) * .5
+    #ssim_value = (ssim_value + 1.) * .5
+    ssim_value = max(ssim_value, 0.0)
 
     # Print the similarity measure
     return ssim_value
