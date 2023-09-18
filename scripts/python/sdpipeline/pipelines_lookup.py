dd658bf50bb846cab86db581c99e23404485f722
Paul Ambrosiussen <paul@ambrosiussen.com>
2023-09-18 11:02:22 +0200

    Update pipelines_lookup.py

--- scripts/python/sdpipeline/pipelines_lookup.py
+++ scripts/python/sdpipeline/pipelines_lookup.py
@@ -2,10 +2,12 @@ from diffusers import (
     StableDiffusionControlNetInpaintPipeline,
     StableDiffusionInpaintPipeline,
     StableDiffusionPipeline,
+    StableDiffusionXLPipeline,
 )
 
 pipelines = {
     "StableDiffusionPipeline": StableDiffusionPipeline,
     "StableDiffusionInpaintPipeline": StableDiffusionInpaintPipeline,
     "StableDiffusionControlNetInpaintPipeline": StableDiffusionControlNetInpaintPipeline,
+    "StableDiffusionXLPipeline": StableDiffusionXLPipeline,
 }
