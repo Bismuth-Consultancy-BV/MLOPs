4d87af863e64b2db466bff7813df111bd7a6e753
Ambrosiussen <paul@ambrosiussen.com>
2025-04-15 14:45:16 +0200

    Fix convert ckpt error

--- scripts/python/sdpipeline/convert_model.py
+++ scripts/python/sdpipeline/convert_model.py
@@ -10,7 +10,7 @@ from diffusers.pipelines.stable_diffusion.convert_from_ckpt import (
 def convert(checkpoint_file, config_file, export_path):
     from_safetensor = checkpoint_file.endswith(".safetensors")
     pipe = download_from_original_stable_diffusion_ckpt(
-        checkpoint_path=checkpoint_file,
+        checkpoint_path_or_dict=checkpoint_file,
         original_config_file=config_file,
         load_safety_checker=False,
         from_safetensors=from_safetensor,
